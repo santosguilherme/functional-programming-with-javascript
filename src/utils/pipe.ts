3ae12dd9bcaff0f9b39ab395516e7c32b3af5fd9
Guilherme Santos <guilherme.santos@doclerholding.com>
2021-04-14 12:59:40 +0200

    pipe implementation

--- src/utils/pipe.ts
+++ src/utils/pipe.ts
@@ -1,1 +1,7 @@
-export const pipe = null;
+export const pipe = (...functions) => {
+    if (!functions || functions.length < 2) {
+        throw new Error('Should provide at least 2 functions');
+    }
+
+    return argument => functions.reduce((result, fn) => fn(result), argument);
+};
